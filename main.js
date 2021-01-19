f23f58fea62d0bff3841d71b42850dca8655a13e
Shreyas-4213 <76523134+Shreyas-4213@users.noreply.github.com>
2021-01-19 16:28:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 status="";
+object=[];
 function preload(){
 img=loadImage('dog_cat.jpg');
 }
@@ -21,23 +22,22 @@ if (error) {
 console.log(error)  
 }
 console.log(results);
+object=results;
 }
 
 function draw(){
 image(img,0,0,640,420);
-fill("#FF0000");
-text("dog",45,75);
-noFill();
-stroke("#FF0000");
-rect(30,60,450,350);
-fill("#34E104");
-text("bowl",225,325);
-noFill();
-stroke("#34E104");
-rect(275,325,120,80);
-fill("#0377FD")
-text("cat",550,85);
-noFill();
-stroke("#0377FD");
-rect(275,67,300,350);
+
+
+if (status !="") {
+for (i = 0; i < object.length; i++) {
+    document.getElementById("status").innerHTML="Status:- Detecting Objects";
+    fill("#FF0000");
+    percent= floor(object[i].confidence * 100);
+    text(object[i].label+""+percent+"%", object[i].x, object[i].y);
+    noFill();
+    stroke("#FF0000");
+    rect(object[i].x, object[i].y, object[i].width, object[i].height);
+}    
+}
 }
